dd8216bd7453ebd279de1627e5cfe92e09abdf1f
Madeleine Johnson <mjohnson3038@gmail.com>
2016-12-16 15:55:28 -0800

    added custom events to views

---
+++ src/app/app.js
@@ -0,0 +1,6 @@
+import ApplicationView from 'app/views/application';
+
+var appView = new ApplicationView({
+  el: '#main',
+  model: application
+});
--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -5,6 +5,14 @@ var BoardView = Backbone.View.extend({
     this.render();
   },
 
+  events: {
+    'click .square': 'playSquare'
+  },
+
+  playSquare: function(e){
+    // To click and play a square in a game of TTT. Need to find a way to pass in the id of the click element.
+  },
+
   render: function() {
     return this;
   }
