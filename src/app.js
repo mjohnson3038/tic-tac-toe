ecc7e9a5a00d4c0b9d0c3f2596f6a62545d4c6d7
Madeleine Johnson <mjohnson3038@gmail.com>
2016-12-19 15:12:13 -0800

    Change name of model to game to match logic

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import ApplicationView from 'app/views/application';
 import Application from 'app/models/application';
 import BoardView from 'app/views/board_view';
 
-var application = new Application();
+var game = new Game();
 
 var appView = new ApplicationView({
   el: 'main',
-  model: application
+  model: game
 });
 
 // appView.render();
